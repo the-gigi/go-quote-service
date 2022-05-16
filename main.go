bae2b1be2c30645d1c6a07f835ff5e63577109ea
Gigi Sayfan <the.gigi@gmail.com>
2022-05-16 10:26:41 -0700

    minor refactoring. tested with remote Render postgres DB

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-    "github.com/the-gigi/go-quote-service/cmd/service"
-    "os"
+	"github.com/the-gigi/go-quote-service/cmd/service"
+	"os"
+)
+
+const (
+	port int = 7777
 )
 
 func main() {
-    port := 7777
-    connectionString := os.Getenv("GO_QUOTE_SERVICE_CONNECTION_STRING")
-    fmt.Println("")
-    err := service.Run(port, connectionString)
-    if err != nil {
-        panic(err)
-    }
+	connectionString := os.Getenv("GO_QUOTE_SERVICE_CONNECTION_STRING")
+	fmt.Println("Listening on port ", port)
+	err := service.Run(port, connectionString)
+	if err != nil {
+		panic(err)
+	}
 }
