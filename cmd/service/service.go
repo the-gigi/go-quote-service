708f20bc142ddc01341be1dbc8088638de8423bd
Gigi Sayfan <the.gigi@gmail.com>
2022-05-29 16:06:49 -0700

    no db

--- cmd/service/service.go
+++ cmd/service/service.go
@@ -3,17 +3,14 @@ package service
 import (
 	"encoding/json"
 	"github.com/gorilla/mux"
-	"io/ioutil"
+	//"io/ioutil"
 	"net/http"
 	"strconv"
-
-	om "github.com/the-gigi/go-quote-service/pkg/object_model"
-	"github.com/the-gigi/go-quote-service/pkg/quote_store"
 )
 
 type Service struct {
-	router     *mux.Router
-	quoteStore om.QuoteStore
+	router *mux.Router
+	//quoteStore om.QuoteStore
 }
 
 func (s *Service) register() {
@@ -23,13 +20,18 @@ func (s *Service) register() {
 
 func (s *Service) HandleGetQuotes(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	quotes, err := s.quoteStore.GetQuotes()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	//quotes, err := s.quoteStore.GetQuotes()
+	//if err != nil {
+	//	http.Error(w, err.Error(), http.StatusInternalServerError)
+	//	return
+	//}
+
+	quotes := []string{
+		"quote-1",
+		"quote-2",
 	}
 
-	err = json.NewEncoder(w).Encode(quotes)
+	err := json.NewEncoder(w).Encode(quotes)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -38,23 +40,23 @@ func (s *Service) HandleGetQuotes(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Service) HandleNewQuote(w http.ResponseWriter, r *http.Request) {
-	if r.Body == nil {
-		http.Error(w, "request body can't be nil", http.StatusBadRequest)
-		return
-	}
-
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	quote := string(body)
-	err = s.quoteStore.AddQuote(quote)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
+	//if r.Body == nil {
+	//	http.Error(w, "request body can't be nil", http.StatusBadRequest)
+	//	return
+	//}
+	//
+	//body, err := ioutil.ReadAll(r.Body)
+	//if err != nil {
+	//	http.Error(w, err.Error(), http.StatusBadRequest)
+	//	return
+	//}
+	//
+	//quote := string(body)
+	//err = s.quoteStore.AddQuote(quote)
+	//if err != nil {
+	//	http.Error(w, err.Error(), http.StatusBadRequest)
+	//	return
+	//}
 
 	return
 }
@@ -69,14 +71,14 @@ func (s *Service) run(port int) {
 }
 
 func Run(port int, connectionString string) (err error) {
-	quoteStore, err := quote_store.NewQuoteStore(connectionString)
-	if err != nil {
-		return
-	}
+	//quoteStore, err := quote_store.NewQuoteStore(connectionString)
+	//if err != nil {
+	//	return
+	//}
 
 	s := Service{
-		router:     mux.NewRouter(),
-		quoteStore: quoteStore,
+		router: mux.NewRouter(),
+		//quoteStore: quoteStore,
 	}
 	s.register()
 	s.run(port)
