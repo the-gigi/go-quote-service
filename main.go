3bb346bfe13f1016113cb542fd7b50b9949cf2ca
Gigi Sayfan <the.gigi@gmail.com>
2022-05-29 16:23:47 -0700

    better error reporting

--- main.go
+++ main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"github.com/the-gigi/go-quote-service/cmd/service"
-	"os"
-	"strconv"
+    "fmt"
+    "github.com/the-gigi/go-quote-service/cmd/service"
+    "os"
+    "strconv"
 )
 
 func main() {
-	servicePort := os.Getenv("GO_QUOTE_SERVICE_PORT")
-	if servicePort == "" {
-		servicePort = "80"
-	}
+    servicePort := os.Getenv("GO_QUOTE_SERVICE_PORT")
+    if servicePort == "" {
+        servicePort = "80"
+    }
 
-	port, err := strconv.Atoi(servicePort)
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
+    port, err := strconv.Atoi(servicePort)
+    if err != nil {
+        fmt.Println(err.Error())
+        os.Exit(1)
+    }
 
-	connectionString := os.Getenv("GO_QUOTE_SERVICE_CONNECTION_STRING")
-	fmt.Println("Listening on port ", port)
-	err = service.Run(port, connectionString)
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
+    connectionString := os.Getenv("GO_QUOTE_SERVICE_CONNECTION_STRING")
+    fmt.Println("Listening on port ", port)
+    err = service.Run(port, connectionString)
+    if err != nil {
+        fmt.Printf("%v\n", err)
+        os.Exit(1)
+    }
 }
